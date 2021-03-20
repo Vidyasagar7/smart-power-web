7dbd511ffffb96102ef130a26c2efa4d73660eb9
Vidyasagar7 <vidsagar1999@gmail.com>
2021-03-20 18:15:46 +0530

    initial back end commit

--- src/Pages/LinkAccount/LinkAccount.js
+++ src/Pages/LinkAccount/LinkAccount.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import  './LinkAccount.css';
-import lightImage from '../../images/Bulb.jpg';
-const LinkAccount = () =>{
-    return(
-        <div className = "Container">
-            <div className = "AccountDetails">
-                <h3>Welcome User,</h3>
-                <p className = "paragraph">You are almost there... Please provide your identification details</p>
-                <span className="meterText">Meter ID</span><br></br>
-                <input type="text" className="textBox" name="email" required /><br></br>
-                <button className = "confrimButton">Confirm</button>
-            </div>
-            <img src ={lightImage} className = "lightimage"></img>
-        </div>
-    );
-}
+import React, { useState } from "react";
+import "./LinkAccount.css";
+import lightImage from "../../images/Bulb.jpg";
+
+const LinkAccount = ({ linkAccount }) => {
+  const [meterId, setMeterId] = useState("");
+  return (
+    <div className="Container">
+      <div className="AccountDetails">
+        <h3>Welcome User,</h3>
+        <p className="paragraph">
+          You are almost there... Please provide your house smart meter serial number
+        </p>
+        <span className="meterText">Meter ID</span>
+        <br></br>
+        <input
+          type="text"
+          value={meterId}
+          className="textBox"
+          name="meterId"
+          onChange={(e) => setMeterId(e.target.value)}
+          required
+        />
+        <br></br>
+        <button className="confrimButton" onClick={() => linkAccount(meterId)}>
+          Confirm
+        </button>
+      </div>
+      <img src={lightImage} className="lightimage"></img>
+    </div>
+  );
+};
 
 export default LinkAccount;
